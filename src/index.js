1b1ab5848979c9d75b723ef5b8519888af2380cc
liruonan <812560550@qq.com>
2017-04-11 12:49:21 +0800

    new modified

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import App from './app'
 import Cube from './objects/cube'
 
 let app = new App()
+// app.add(new Cube({
+//   width: 5,
+//   height: 5,
+//   depth: 5,
+//   color: 0x102951
+// }))
+// 
 app.add(new Cube({
-  width: 5,
-  height: 5,
+  width: 10,
+  height: 10,
   depth: 5,
   color: 0x102951
 }))
