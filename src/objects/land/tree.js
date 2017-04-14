2209052382aa00f53893b7905bca6135e1db27b7
阮骁扬 <ruanxiaoyang@MacBook-Pro.local>
2017-04-14 15:12:00 +0800

    question in treemesh

--- src/objects/land/tree.js
+++ src/objects/land/tree.js
@@ -1,18 +1,30 @@
 import * as THREE from 'three'
 
 class Tree{
-	Constructor(){
+	constructor(){
 		var dae;
+		console.log(5)
 		var loader = new THREE.ColladaLoader();
 		loader.options.convertUpAxis = true;
-		loader.load('./models/Tree.dae',function(collada){
-			this.dae = collada.scene
-		})
+		loader.load('../../models/Tree.dae',function(collada){
+	    	this.dae = collada.scene
+	    	console.log(2)
+			this.dae.traverse( function ( child ) {
+				if ( child instanceof THREE.Mesh ) {
+					console.log(1)
+					child.geometry.computeFaceNormals();
+					child.material.shading = THREE.FlatShading;
+				}
+			} );
+		
+		console.log(3)
 		this.dae.scale.x = dae.scale.y = dae.scale.z = 0.002;
 		this.dae.updateMatrix();
-		
+		});
 	}
   getMesh() {
+  	console.log(4)
+
   	return this.dae
   }
 }
