d15317f6f31e3fe440d770b72ee91fbbc81cb40d
Cribug <723478738@qq,com>
2017-04-14 20:59:57 +0800

    test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Land from './objects/land/land'
 import Role from './objects/role/role'
 import Star from './objects/sky/star'
 import Sun from './objects/sky/sun'
+import Boy from './objects/boy/boy'
 import './style/app.scss'
 
 import Audio from './objects/audio/Audio'
@@ -11,16 +12,21 @@ import Audio from './objects/audio/Audio'
 let app = new App()
 
 app.add(new Land({
-	color: config.blue
+    color: config.blue
 }))
 app.add(new Role({
-	color: config.red
+    color: config.red
 }))
 app.add(new Star({
-	color: config.yellow
+    color: config.yellow
 }))
 
 app.add(new Sun({
-	color1: config.red,
-	color2: config.yellow
+    color1: config.red,
+    color2: config.yellow
+}))
+app.add(new Boy({
+    color: 0xf7d9aa,
+    size: 4,
+    pt: [20, 90, 30]
 }))
