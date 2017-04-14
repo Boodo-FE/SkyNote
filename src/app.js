a7a383c48fe8c87e2eaaca2116b4311035330791
stephenliu <stephenlyao@yeah.net>
2017-04-14 21:20:23 +0800

    land

--- src/app.js
+++ src/app.js
@@ -1,18 +1,39 @@
 import * as THREE from 'three'
+import config from './constant/index'
 import Land from './objects/land/land'
 import Role from './objects/role/role'
 import Star from './objects/sky/star'
 import Sun from './objects/sky/sun'
+import Boy from './objects/boy/boy'
+import Audio from './objects/audio/Audio'
+
+
 class App {
   constructor() {
-    this.objects = []
+
+    this.land = new Land({ color: config.blue})
+    this.role = new Role({ color: config.red })
+    this.star = new Star({ color: config.yellow })
+    this.sun  = new Sun({ color1: config.red, color2: config.yellow })
+    // this.boy  = new Boy({ color: 0xf7d9aa,  size: 4, pt: [20, 90, 30] })
+
     this.createScene()
     this.createLights()
+    this.addMesh()   
+    this.handleEvents()
+  }
+
+  addMesh() {
+    this.add(this.land)
+    this.add(this.role)
+    this.add(this.star)
+    this.add(this.sun)
+    // this.add(this.boy)
   }
   
   createScene() {
     this.scene = new THREE.Scene()
-    this.scene.fog = new THREE.Fog(0xf7d9aa, 100, 900)
+    // this.scene.fog = new THREE.Fog(0xf7d9aa, 100, 900)
     this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 2000)
     this.camera.position.x = 0;
     this.camera.position.z = 200;
@@ -32,7 +53,7 @@ class App {
   createLights() {
     this.hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9)
     this.shadowLight = new THREE.DirectionalLight(0xffffff, .9)
-  
+
     this.shadowLight.position.set(150, 300, 350)
     this.shadowLight.castShadow = true
     this.shadowLight.shadow.camera.left = -400
@@ -46,35 +67,38 @@ class App {
 
     this.scene.add(this.hemisphereLight)
     this.scene.add(this.shadowLight)
+    
   }
 
   render() {
     requestAnimationFrame(() => {
       this.render()
     })
-    
-    this.objects.forEach((object) => {
-      if(object instanceof Land) {
-        document.addEventListener('keydown', (e) => {
-          e.preventDefault()
-          if(e.which === 39) {
-            object.update()
-          }
-        })       
-      }else if(object instanceof Sun){
-        object.update()
-      }else if(object instanceof Star){
-        object.update()
-      }
-      
-    })
-    
+    // this.boy.update()
+    this.star.update()
+    this.sun.update()    
     this.renderer.render(this.scene, this.camera)
   }
   
   add(mesh) {
-    this.objects.push(mesh)
     this.scene.add(mesh.getMesh())
+  }
+
+  handleEvents() {
+    let that = this
+    document.addEventListener('keyup', function(e) {
+      e.preventDefault()
+      if(e.which === 38) {
+        that.role.update()
+      }
+    }, false)
+    document.addEventListener('mousemove', function(e) {
+      e.preventDefault()
+      that.land.update(that.role)
+      // if(e.which === 39) {
+      //   that.land.update()
+      // }
+    }, false)
   }
 
   handleWindowResize() {
--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
 import App from './app'
-import config from './constant/index'
-import Land from './objects/land/land'
-import Role from './objects/role/role'
-import Star from './objects/sky/star'
-import Sun from './objects/sky/sun'
-import Boy from './objects/boy/boy'
 import './style/app.scss'
 
-import Audio from './objects/audio/Audio'
 
 let app = new App()
 
-app.add(new Land({
-    color: config.blue
-}))
-app.add(new Role({
-    color: config.red
-}))
-app.add(new Star({
-    color: config.yellow
-}))
-
-app.add(new Sun({
-    color1: config.red,
-    color2: config.yellow
-}))
-app.add(new Boy({
-    color: 0xf7d9aa,
-    size: 4,
-    pt: [20, 90, 30]
-}))
